c2a1bf46a821eed50ac3fbf4b3e25d3833ece6a1
SteffenKloempges <steffen.kloempges@gmail.com>
2021-01-20 20:46:46 +0100

    addJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 function popUp() {
     console.log("it works")
+    console.log(document.getElementById("emailInput").value)
+    document.getElementById("popUp-container").style.display = 'block'
+    document.getElementById("subscribe1").style.visibility = 'hidden'
+    document.getElementById("subscribe2").style.visibility = 'hidden'
+    document.getElementById("subscribe3").style.visibility = 'hidden'
+}
+
+function backtowebsite() {
+    document.getElementById("popUp-container").style.display = 'none'
+    document.getElementById("subscribe1").style.visibility = 'visible'
+    document.getElementById("subscribe2").style.visibility = 'visible'
+    document.getElementById("subscribe3").style.visibility = 'visible'
 }
